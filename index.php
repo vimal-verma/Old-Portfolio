da99e06cea27eb28b3313b39c7cfb2407d2beaac
VIMAL KUMAR <46487476+vimalverma558@users.noreply.github.com>
2020-02-06 19:46:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>vimal</title>
+</head>
+<body>
+
+
+
+<?php 
+include 'lib/db.php';
+include 'lib/header.php';
+
+
+
+include 'lib/main.php';
+include 'admin/login.php';
+include 'lib/post.php';
+include 'lib/footer.php';
+?>
+
+
+
+</body>
+</html>

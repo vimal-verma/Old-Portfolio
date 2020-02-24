dbe8a88b157a1dbfa4100aec3efd1e4f43fe7a00
VIMAL KUMAR <kumarvimal558@gmail.com>
2020-02-24 12:01:50 +0530

    vimal

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // $("#man").css({"display": "none"});
 
 $("#mobile").hide();
+
+  function project() {
+    var myWindow = window.open("", "MsgWindow", "width=800%,height=500%");
+    myWindow.document.write(" <h1>Hello world</h1> ");
+  }
+
+  function About() {
+    var myWindow = window.open("", "MsgWindow", "width=800%,height=500%");
+    myWindow.document.write("<p>Hi!, i am vimal</p><h1>vimal</h1> <a>face</a>");
+  }  
+
+
+  function contact() {
+    var myWindow = window.open("", "MsgWindow", "width=800%,height=500%");
+    myWindow.document.write("<p>Hi!, i am vimal</p>");
+  }  
